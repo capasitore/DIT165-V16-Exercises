0d538a53de53515de358886ea6d8f93fc31c7ff3
Phub71 <pooriya.balavi@gmail.com>
2016-02-03 14:20:25 +0100

    Update exerc_2_2.c

--- Work_package nr 2/exerc_2_2.c
+++ Work_package nr 2/exerc_2_2.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 20
+#define MAX 8
 
 int main(void) {
-	 int arr[MAX];
-		  int i, *ptr;
+	 int arr[MAX]= {10,21,99,65,8,38,90,11};
+		  int *ptr = NULL;
 		  ptr = arr[MAX];
 
-		  srand(time(NULL));
 
-		  for(i=0; i<=MAX; i++){
-			  arr[i] = rand()%100;
-		  	}
-
-		  printf("The value of label array ( address) is : %x\n", ptr);
+		  printf("The value of label array ( address) is : %p\n", ptr);
 		  printf("the first value in the array is : %d\n", arr[0]);
 		  printf("The size of an integer(number of bytes) is: %ld\n", sizeof(int));
 		  printf("The size of the whole array is: %ld\n", sizeof(arr));
